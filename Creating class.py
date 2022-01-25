e08ad536076c675a26b1001d6b9e323aada84c02
anish2026 <98420946+anish2026@users.noreply.github.com>
2022-01-26 00:26:04 +0530

    Create Creating class.py

---
+++ Creating class.py
@@ -0,0 +1,8 @@
+# Ques 1 Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+
+class vehicle():
+    
+    def _init_(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+x=vehicle(100,35)
